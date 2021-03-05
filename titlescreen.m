57e4cb1660c5fb4b8574ea93643d4ffa25ea1d3d
jacquesnissen <jacquesnissen@jacquess-air.devices.brown.edu>
2021-03-05 16:04:03 -0500

    titlescreen + hardcoded first block (no randomization)

--- titlescreen.m
+++ titlescreen.m
@@ -19,16 +19,15 @@ screenNumber = max(screens);
 % Define white, black, and grey using the luminance values for the display.
 white = WhiteIndex(screenNumber);
 black = BlackIndex(screenNumber);
-grey = white/2;
+grey = white/2;mhh
 
 [window, windowRect] = PsychImaging('OpenWindow', screenNumber, grey);
 Screen('TextSize', window, 20);
 Screen('TextFont', window, 'Courier');
-DrawFormattedText(window, 'Hello! Thank you for participating in our experiment. Here’s what you need to know: \n 1. There will be four rounds. \n 2. In each round, you will see a string of seven two-digit numbers that you will have 15 seconds to memorize. \n 3. After those 15 seconds, you will have a 10 second break where you will be asked to count backwards by 3s from a certain number. \n 4. You will then be prompted to recall the string of numbers, in order, to the best of your ability. \n 5. Once you’ve finished all four rounds, you will see a display of your results. \n Press any key to continue and get started!', 'center', 'center', [1 1 1]);
+DrawFormattedText(window, 'Hello! Thank you for participating in our experiment. Here’s what you need to know: \n\n\n 1. There will be four rounds. \n 2. In each round, you will see a string of seven two-digit numbers that you will have 15 seconds to memorize. \n 3. After those 15 seconds, you will have a 10 second break where you will be asked to count backwards by 3s from a certain number. \n 4. You will then be prompted to recall the string of numbers, in order, to the best of your ability. \n 5. Once you’ve finished all four rounds, you will see a display of your results. \n Press any key to continue and get started!', 'center', 'center', [1 1 1]);
 Screen('Flip', window);
 KbStrokeWait;
 
-Screen('Flip', window);
 Screen('TextSize', window, 40);
 Screen('TextFont', window, 'Courier');
 DrawFormattedText(window, '34, 19, 72, 51, 83, 86, 17','center','center',[1,0,0]); 

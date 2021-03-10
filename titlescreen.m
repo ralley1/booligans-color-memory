cb71948e4ceb2d0ad8ca22913b974d77912f2e1b
rhianmaguire <rhianmaguire@localhost>
2021-03-09 21:39:09 -0500

    full version of working code! missing demo and a few comments though.

--- titlescreen.m
+++ titlescreen.m
@@ -2,14 +2,12 @@
 sca;
 close all;
 clear all;
-% Skip some tests in case your initial ScreenTest command failed. Ignore
-%otherwise
+% Skip some tests in case your initial ScreenTest command failed. Ignore otherwise
 Screen('Preference', 'SkipSyncTests', 1);
 % Default settings for Psychtoolbox
 PsychDefaultSetup(2);
-
-%Get the screen numbers. This gives us a number for each of the screens
-%attached to your computer. The default should be 0.
+ 
+%Get the screen numbers.
 screens = Screen('Screens');
 % Find the maximum screen number - if there are two screens attached to
 %your computer, the one with the higher number will be the external display.
@@ -19,56 +17,62 @@ screenNumber = max(screens);
 white = WhiteIndex(screenNumber);
 noir = BlackIndex(screenNumber);
 grey = white/2;
-
+ 
 %Initializes colors
 red = [1 0 0];
 green = [0 1 0];
 blue = [0 0 1];
 black = [0 0 0];
-
+ 
 %Creates array of colors, then randomizes order
 colRect = {red green blue black};
 col_rand = colRect(randperm(length(colRect)));
-
-%creates blank arrays for storing response time and accuracy
+ 
+%creates blank arrays for storing response time, accuracy, colors
 rT = [];
 accuracy = [];
-
+colors = [];
+ 
 %Displays intro page
 [window, windowRect] = PsychImaging('OpenWindow', screenNumber, grey);
 Screen('TextSize', window, 20);
 Screen('TextFont', window, 'Courier');
-DrawFormattedText(window, 'Hello! Thank you for participating in our experiment. Here’s what you need to know: \n\n\n 1. There will be four rounds. \n 2. In each round, you will see a string of seven two-digit numbers that you will have 15 seconds to memorize. \n 3. After those 15 seconds, you will have a 10 second break where you will be asked to count backwards by 3s from a certain number. \n 4. You will then be prompted to recall the string of numbers, in order, to the best of your ability. \n 5. Once you’ve finished all four rounds, you will see a display of your results. \n Press any key to continue and get started!', 'center', 'center', [1 1 1]);
+DrawFormattedText(window, 'Hello! Thank you for participating in our experiment. Hereâ€™s what you need to know: \n\n\n 1. There will be four rounds. \n 2. In each round, you will see a string of seven two-digit numbers that you will have 15 seconds to memorize. \n 3. After those 15 seconds, you will have a 10 second break where \n you will be asked to count backwards by 3s from a certain number. \n 4. You will then be prompted to recall the string of numbers to the best of your ability. \n 5. Once youâ€™ve finished all four rounds, you will see a display of your results. \n \n \n Press Enter to continue and get started!', 'center', 'center', [1 1 1]);
 Screen('Flip', window);
 KbStrokeWait;
-
-%Shows each test screen in a loop
+ 
+%Loop for set of 4 blocks/trials, one for each color
 for ii = 1:4
     str_dig = num2str(randi([10 99], 1, 7));
-    
+   
+    %memorization screen
     Screen('TextSize', window, 40);
     Screen('TextFont', window, 'Courier');
     DrawFormattedText(window, str_dig ,'center','center', col_rand{ii}); 
     Screen('Flip', window);
-    WaitSecs(10);
+    WaitSecs(15);
     
+    %intermediary screen
     Screen('TextSize', window, 30);
     Screen('TextFont', window, 'Courier');
     DrawFormattedText(window, 'Please count backwards from 60 by threes until the next screen is displayed.', 'center', 'center', [1 1 1]);
     Screen('Flip', window);
-    WaitSecs(1);
+    WaitSecs(10);
     
+    %response screen
     Screen('TextSize', window, 40);
     Screen('TextFont', window, 'Courier');
-    DrawFormattedText(window, 'Please type as much of the string you remember, in order. \n (Hit Enter when finished!)','center','center',[1,1,1]);
+    DrawFormattedText(window, 'Please type as much of the string you remember. \n (Hit Enter when finished!)','center','center',[1,1,1]);
     tic;
-    %reply = Ask(window, 'Your Input: ', [1 1 1], grey, 'GetChar', RectLeft, RectBottom);
-    reply = Ask(window, 'Your Input: ', [1 1 1], grey, 'GetChar', [600 400 400 600]);
+    reply = 0;
+    reply = Ask(window, 'Your Input: ', [1 1 1], grey, 'GetChar', RectLeft, RectBottom);
     elapsedTime = toc;
     Screen('TextSize', window, 40);
     Screen('TextFont', window, 'Courier');
-    DrawFormattedText(window, 'Press Any Key','center','center',[1,1,1]);
-
+    DrawFormattedText(window, 'Press Enter to continue','center','center',[1,1,1]);
+     
+    %collecting response time, accuracy, and colors to then
+    %create array of info across all trials
     rT = [rT, elapsedTime];
     x = strsplit(str_dig);
     y = strsplit(reply);
@@ -82,10 +86,28 @@ for ii = 1:4
     Screen('Flip', window);
     KbStrokeWait
     
+    
+    if col_rand{ii} == black
+        col_loop = "black";
+    elseif col_rand{ii} == red
+        col_loop = "red";
+    elseif col_rand{ii} == green
+        col_loop = "green";
+    else
+        col_loop = "blue";
+    end
+    colors = [colors, col_loop];
 end
-
-%Results screen with plot of reaction time (rT) and accuracy (accuracy)
-colors = {col_rand};
+ 
+%final screen
+Screen('TextSize', window, 25);
+Screen('TextFont', window, 'Courier');
+DrawFormattedText(window, 'Thanks for playing! \n Press Enter to exit the screen and see your results', 'center', 'center', [1 1 1]);
+Screen('Flip', window);
+KbStrokeWait;
+ 
+ 
+%Results: plot of reaction time (rT) and accuracy (accuracy)
 a = categorical(colors);
 a = reordercats(a, colors);
 b1 = accuracy;
@@ -95,6 +117,7 @@ results = figure;
  
 subplot(1,2,1);
 bar(a, b1, 0.75, 'k');
+ylim([0,7]);
 title('Accuracy');
 xlabel('color of text');
 ylabel('number of correct responses');
@@ -109,70 +132,5 @@ figureTexture = Screen('MakeTexture', window, results);
 Screen('DrawTexture', window, figureTexture, [], [], 0);
 Screen('Flip', window);
 KbStrokeWait;
-
-Screen('TextSize', window, 40);
-Screen('TextFont', window, 'Courier');
-DrawFormattedText(window, 'Thanks for playing!', 'center', 'center', [1 1 1]);
-Screen('Flip', window);
-KbStrokeWait;
-
-%[string,terminatorChar] = GetEchoString(window,'type!','center','center',[1,1,1],[],[useKbCheck == 0],[deviceIndex],[untilTime == inf]);
-    
-
-
-
-%{
-Screen('TextSize', window, 40);
-Screen('TextFont', window, 'Courier');
-DrawFormattedText(window, 'Please type as much of the string you remember, in order. \n (Hit Enter when finished!)','center','center',[1,1,1]);
-reply = Ask(window, 'Your Input: ', [1 1 1], grey, 'GetChar', RectLeft, RectBottom);
-Screen('Flip', window);
-KbStrokeWait;
-
-Screen('TextSize', window, 40);
-Screen('TextFont', window, 'Courier');
-DrawFormattedText(window, 'this is a check to make sure reply stores the right data' ,'top','center',[1,1,1]);
-DrawFormattedText(window, reply ,'center','center',[1,1,1]);
-
-Screen('Flip', window);
-KbStrokeWait;
-
-%}
-
-%{
-
-%Green Block
-Screen('TextSize', window, 40);
-Screen('TextFont', window, 'Courier');
-DrawFormattedText(window, '55, 23, 87, 46, 92, 15, 39','center','center',[0,1,0]); 
-Screen('Flip', window);
-KbStrokeWait;
-
-%Blue Block
-Screen('TextSize', window, 40);
-Screen('TextFont', window, 'Courier');
-DrawFormattedText(window, '64, 13, 75, 52, 97, 22, 48','center','center',[0,0,1]); 
-Screen('Flip', window);
-KbStrokeWait;
-
-%Black Block
-Screen('TextSize', window, 40);
-Screen('TextFont', window, 'Courier');
-DrawFormattedText(window, '29, 16, 85, 61, 33, 86, 96','center','center',[0,0,0]); 
-Screen('Flip', window);
-KbStrokeWait;
-
-%for i = 1:4
-	%Display one of the color blocks
-	%display intermediate screen asking individual to count backwards by 3s from randi > 30
-	% display screen asking individual to type in recalled string of numbers in order, recording the time it takes to submit as well as how accurate their input is
-        %reply = Ask(window, ‘Please type as much of the string you remember, in order.’, [1 1 1], [background color], ‘GetString’, ‘center’, ‘bottom’)
-	%loop back through until all blocks are presented
-%end
-%Results screen with plot of reaction time and accuracy
-%Screen saying thank you for playing!
-%}
-
-
-
+ 
 sca;
